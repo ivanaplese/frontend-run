3717720e88ec7eccd4e4c74795a8d3e1785035d3
ivanaplese <ivanaplese123@gmail.com>
2024-08-22 20:03:01 +0200

    dodano ime aplikacije u navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/signup">Signup</router-link>
-    </div>
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <!-- Naziv aplikacije -->
+        <router-link to="/" class="navbar-brand text-orange">run!</router-link>
+
+        <!-- Linkovi -->
+        <div class="collapse navbar-collapse justify-content-end">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/signup" class="nav-link">Signup</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
@@ -14,20 +30,27 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
+  
 }
 
 #nav {
-  padding: 30px;
+  padding: 10px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    
 
     &.router-link-exact-active {
       color: orange;
     }
+  }
+
+  .text-orange {
+    color: #f58634 !important; /* Narančasta boja za naziv aplikacije */
+    font-size: 1.5rem;
+    font-weight: bold;
   }
 }
 </style>
