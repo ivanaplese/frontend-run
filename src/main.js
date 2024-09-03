664ef2e58e8ee8506c1f5f133ab760d8a5be6a8e
ivanaplese <ivanaplese123@gmail.com>
2024-09-03 20:17:36 +0200

    update import vue-croppa za css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Croppa from "vue-croppa";
+import "vue-croppa/dist/vue-croppa.css";
+
 
 createApp(App).use(router).mount("#app");
 createApp.use(Croppa);
