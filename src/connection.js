8a86a8e50efeced7f3b85b6ed6a956b5c26c4dc7
ivanaplese <ivanaplese123@gmail.com>
2025-01-31 13:02:32 +0100

    priprema za vercel, npm run buildddd

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const apiClient = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL || "https://backend-run.vercel.app/",
+
     headers: {
         "Content-Type": "application/json",
     },
