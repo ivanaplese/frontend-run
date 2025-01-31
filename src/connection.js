842e131afd1db694ff758cc5707fbcd3dc743885
ivanaplese <ivanaplese123@gmail.com>
2025-01-31 13:35:25 +0100

    ajd pliz proradi

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-console.log("VITE_API_URL:", import.meta.env.VITE_API_URL); // Debug log
+// console.log("VITE_API_URL:", import.meta.env.VITE_API_URL); // Debug log
 
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "https://backend-run.vercel.app",
