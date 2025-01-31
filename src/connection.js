9491348073ceec9062903e22174da4e0b0f3f64e
ivanaplese <ivanaplese123@gmail.com>
2025-01-31 13:31:51 +0100

    neće mi deploy frontenda

--- src/connection.js
+++ src/connection.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("VITE_API_URL:", import.meta.env.VITE_API_URL); // Debug log
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://backend-run.vercel.app/",
+    baseURL: import.meta.env.VITE_API_URL || "https://backend-run.vercel.app",
     headers: {
         "Content-Type": "application/json",
     },
