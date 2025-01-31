ce305cfde953e415f608edf86227f9efd596a314
ivanaplese <ivanaplese123@gmail.com>
2025-01-31 13:51:27 +0100

    nikolija i relja

--- src/connection.js
+++ src/connection.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-console.log("import.meta.env:", import.meta.env); // Debug log za provjeru
-console.log("VITE_API_URL:", import.meta.env.VITE_API_URL); // Debug log
+console.log("VUE_APP_API_URL:", process.env.VUE_APP_API_URL); // Debug log
 
-const API_URL = import.meta.env.VITE_API_URL || "https://backend-run.vercel.app";
+const API_URL = process.env.VUE_APP_API_URL || "https://backend-run.vercel.app";
 
 console.log("Final API_URL:", API_URL); // Debug log
 
