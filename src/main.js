01f372bc8e4952fdca18b932faee318e97ce6c7f
ivanaplese <ivanaplese123@gmail.com>
2025-02-01 18:12:19 +0100

    ups?

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Croppa from "vue-croppa";
-import { auth } from "./firebase";
 import "vue-croppa/dist/vue-croppa.css";
 
 const app = createApp(App);
@@ -20,5 +19,3 @@ auth.onAuthStateChanged((user) => {
 
 app.use(router).use(Croppa).mount("#app");
 
-//app.mount("#app"): Montira aplikaciju na HTML element s ID-jem #app, što znači da će se Vue aplikacija prikazati unutar tog elementa.
-//u mainu se kreira vue aplikacija koristeci app.vue i dodaje se ruter store croppa
